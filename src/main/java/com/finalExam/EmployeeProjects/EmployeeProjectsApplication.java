8f0639e573ba721349152900fc350fcfab702f2a
NevenaKoynarcheva <nevena19k@gmail.com>
2023-12-17 10:40:39 +0200

    create manually database

--- src/main/java/com/finalExam/EmployeeProjects/EmployeeProjectsApplication.java
+++ src/main/java/com/finalExam/EmployeeProjects/EmployeeProjectsApplication.java
@@ -3,10 +3,44 @@ package com.finalExam.EmployeeProjects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class EmployeeProjectsApplication {
 
+
 	public static void main(String[] args) {
+		Connection connection = null;
+		Statement statement = null;
+		try {
+
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "1234");
+			statement = connection.createStatement();
+			statement.executeQuery("SELECT count(*) FROM pg_database WHERE datname = 'employeeProject'");
+			ResultSet resultSet = statement.getResultSet();
+			resultSet.next();
+			int count = resultSet.getInt(1);
+
+			if (count <= 0) {
+				statement.executeUpdate("CREATE DATABASE employeeProject");
+
+			}
+		} catch (SQLException e) {
+
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+
+				}
+				if (connection != null) {
+
+					connection.close();
+				}
+			}catch (SQLException e){
+
+			}
+		}
 		SpringApplication.run(EmployeeProjectsApplication.class, args);
 	}
 
